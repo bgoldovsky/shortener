f5377e47a80586b1bde1e408f12a7d34e3ed806c
Boris Goldovskiy <bbgoldovskiy@avito.ru>
2022-06-16 13:24:57 +0300

    SP-1 vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,20 +4,20 @@ import "os"
 
 // ServerAddress Возвращает адрес HTTP сервера
 func ServerAddress() string {
-	p := os.Getenv("SERVER_ADDRESS")
-	if p == "" {
-		p = ":8080"
+	address := os.Getenv("SERVER_ADDRESS")
+	if address == "" {
+		address = ":8080"
 	}
 
-	return p
+	return address
 }
 
 // BaseURL Возвращает хост для генерации сокращенного URL
 func BaseURL() string {
-	h := os.Getenv("BASE_URL")
-	if h == "" {
-		h = "http://localhost:8080"
+	url := os.Getenv("BASE_URL")
+	if url == "" {
+		url = "http://localhost:8080"
 	}
 
-	return h
+	return url
 }
